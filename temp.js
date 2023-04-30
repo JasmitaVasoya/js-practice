7d089deb192983002fc6e6afeea1110a47114452
JasmitaVasoya <jasmitavasoya@gmail.com>
2023-04-30 19:49:01 +0530

    practice of array methods in js

---
+++ temp.js
@@ -0,0 +1,30 @@
+
+// problem 1
+const arr1 = [1,2,3,2,4,3]
+const newArr1 = []
+arr1.forEach((el)=>{
+    if(!newArr1.includes(el))
+    newArr1.push(el)
+})
+
+// problem 2
+const lettersArr = ['a','b','c','d','e','f','g','h','i','j','k','l','m','n','o','p','q','r','s','t','u','v','w','x','y','z']
+
+const arr2 = ['a','b','c','d','u']
+
+const newArr2 = arr2.map((el)=>lettersArr.indexOf(el)+1)
+
+
+// problem 3
+const arr3 = [false,1,0,1,2,0,1,3,'a']
+const temp1= [],temp2 = []
+ arr3.forEach((el)=> el === 0? temp1.push(el) : temp2.push(el))
+const newArr3 = temp2.concat(temp1)
+
+
+// problem 4
+const arr4 = [{name:'abc',age:20},{name:'xyz',age:30}]
+const newArr4 = arr4.map((el)=>el.name)
+
+
+console.log(newArr1,newArr2,newArr3,newArr4)
